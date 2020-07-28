8f79919634d347b7135892634a73e06dbdde878c
Sam Bianchi <dr.freeman879@gmail.com>
2020-07-28 15:01:15 -0400

    init

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace falloutSolver
 {
-    partial class Form1
+    partial class Solver
     {
         /// <summary>
         /// Required designer variable.
@@ -28,123 +28,232 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Solver));
+            this.slv = new System.Windows.Forms.Button();
+            this.slvd = new System.Windows.Forms.Button();
+            this.word0 = new System.Windows.Forms.TextBox();
+            this.word1 = new System.Windows.Forms.TextBox();
+            this.word2 = new System.Windows.Forms.TextBox();
+            this.word3 = new System.Windows.Forms.TextBox();
+            this.word4 = new System.Windows.Forms.TextBox();
+            this.word5 = new System.Windows.Forms.TextBox();
+            this.word6 = new System.Windows.Forms.TextBox();
+            this.word7 = new System.Windows.Forms.TextBox();
+            this.word8 = new System.Windows.Forms.TextBox();
+            this.word9 = new System.Windows.Forms.TextBox();
+            this.word10 = new System.Windows.Forms.TextBox();
+            this.word11 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.guessDisplay = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.corrNum = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.corrNum)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // slv
             // 
-            this.textBox1.Location = new System.Drawing.Point(0, 0);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(300, 20);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.slv.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.slv.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.slv.Location = new System.Drawing.Point(12, 12);
+            this.slv.Name = "slv";
+            this.slv.Size = new System.Drawing.Size(75, 23);
+            this.slv.TabIndex = 1;
+            this.slv.Text = "Solve";
+            this.slv.UseVisualStyleBackColor = false;
+            this.slv.Click += new System.EventHandler(this.slv_Click);
             // 
-            // button1
+            // slvd
             // 
-            this.button1.Location = new System.Drawing.Point(306, -2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Enter!";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.slvd.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.slvd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.slvd.Location = new System.Drawing.Point(93, 12);
+            this.slvd.Name = "slvd";
+            this.slvd.Size = new System.Drawing.Size(75, 23);
+            this.slvd.TabIndex = 2;
+            this.slvd.Text = "Solved";
+            this.slvd.UseVisualStyleBackColor = false;
+            this.slvd.Click += new System.EventHandler(this.button7_Click);
             // 
-            // button2
+            // word0
             // 
-            this.button2.Location = new System.Drawing.Point(558, -3);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(120, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "How many right?";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.word0.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.word0.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.word0.Location = new System.Drawing.Point(12, 64);
+            this.word0.Name = "word0";
+            this.word0.Size = new System.Drawing.Size(119, 20);
+            this.word0.TabIndex = 3;
             // 
-            // label1
+            // word1
             // 
-            this.label1.Location = new System.Drawing.Point(-3, 52);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(351, 389);
-            this.label1.TabIndex = 4;
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.word1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.word1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.word1.Location = new System.Drawing.Point(12, 90);
+            this.word1.Name = "word1";
+            this.word1.Size = new System.Drawing.Size(119, 20);
+            this.word1.TabIndex = 4;
             // 
-            // label2
+            // word2
             // 
-            this.label2.Location = new System.Drawing.Point(513, 24);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(158, 23);
-            this.label2.TabIndex = 5;
-            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.word2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.word2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.word2.Location = new System.Drawing.Point(12, 116);
+            this.word2.Name = "word2";
+            this.word2.Size = new System.Drawing.Size(119, 20);
+            this.word2.TabIndex = 5;
             // 
-            // label3
+            // word3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(452, 28);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(55, 13);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Answer is:";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
+            this.word3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.word3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.word3.Location = new System.Drawing.Point(12, 142);
+            this.word3.Name = "word3";
+            this.word3.Size = new System.Drawing.Size(119, 20);
+            this.word3.TabIndex = 6;
             // 
-            // button3
+            // word4
             // 
-            this.button3.Location = new System.Drawing.Point(306, 57);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 7;
-            this.button3.Text = "Solved!";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.word4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.word4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.word4.Location = new System.Drawing.Point(12, 168);
+            this.word4.Name = "word4";
+            this.word4.Size = new System.Drawing.Size(119, 20);
+            this.word4.TabIndex = 7;
             // 
-            // button4
+            // word5
             // 
-            this.button4.Location = new System.Drawing.Point(306, 28);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 8;
-            this.button4.Text = "Solve";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.word5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.word5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.word5.Location = new System.Drawing.Point(12, 194);
+            this.word5.Name = "word5";
+            this.word5.Size = new System.Drawing.Size(119, 20);
+            this.word5.TabIndex = 8;
             // 
-            // numericUpDown1
+            // word6
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(455, 0);
-            this.numericUpDown1.Maximum = new decimal(new int[] {
-            15,
+            this.word6.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.word6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.word6.Location = new System.Drawing.Point(12, 220);
+            this.word6.Name = "word6";
+            this.word6.Size = new System.Drawing.Size(119, 20);
+            this.word6.TabIndex = 9;
+            // 
+            // word7
+            // 
+            this.word7.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.word7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.word7.Location = new System.Drawing.Point(12, 246);
+            this.word7.Name = "word7";
+            this.word7.Size = new System.Drawing.Size(119, 20);
+            this.word7.TabIndex = 10;
+            // 
+            // word8
+            // 
+            this.word8.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.word8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.word8.Location = new System.Drawing.Point(12, 272);
+            this.word8.Name = "word8";
+            this.word8.Size = new System.Drawing.Size(119, 20);
+            this.word8.TabIndex = 11;
+            this.word8.TextChanged += new System.EventHandler(this.word8_TextChanged);
+            // 
+            // word9
+            // 
+            this.word9.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.word9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.word9.Location = new System.Drawing.Point(12, 298);
+            this.word9.Name = "word9";
+            this.word9.Size = new System.Drawing.Size(119, 20);
+            this.word9.TabIndex = 12;
+            this.word9.TextChanged += new System.EventHandler(this.word9_TextChanged);
+            // 
+            // word10
+            // 
+            this.word10.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.word10.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.word10.Location = new System.Drawing.Point(12, 324);
+            this.word10.Name = "word10";
+            this.word10.Size = new System.Drawing.Size(119, 20);
+            this.word10.TabIndex = 13;
+            // 
+            // word11
+            // 
+            this.word11.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.word11.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.word11.Location = new System.Drawing.Point(12, 350);
+            this.word11.Name = "word11";
+            this.word11.Size = new System.Drawing.Size(119, 20);
+            this.word11.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(268, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 14);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Guess:";
+            // 
+            // guessDisplay
+            // 
+            this.guessDisplay.Location = new System.Drawing.Point(326, 21);
+            this.guessDisplay.Name = "guessDisplay";
+            this.guessDisplay.Size = new System.Drawing.Size(101, 14);
+            this.guessDisplay.TabIndex = 16;
+            this.guessDisplay.Click += new System.EventHandler(this.guessDisplay_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(268, 48);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(133, 14);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "How many correct?:";
+            // 
+            // corrNum
+            // 
+            this.corrNum.BackColor = System.Drawing.SystemColors.InfoText;
+            this.corrNum.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.corrNum.Location = new System.Drawing.Point(407, 46);
+            this.corrNum.Maximum = new decimal(new int[] {
+            14,
             0,
             0,
             0});
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
-            this.numericUpDown1.TabIndex = 9;
-            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            this.corrNum.Name = "corrNum";
+            this.corrNum.Size = new System.Drawing.Size(45, 20);
+            this.corrNum.TabIndex = 18;
             // 
-            // Form1
+            // Solver
             // 
-            this.AcceptButton = this.button1;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.numericUpDown1);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(593, 412);
+            this.Controls.Add(this.corrNum);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.guessDisplay);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.word11);
+            this.Controls.Add(this.word10);
+            this.Controls.Add(this.word9);
+            this.Controls.Add(this.word8);
+            this.Controls.Add(this.word7);
+            this.Controls.Add(this.word6);
+            this.Controls.Add(this.word5);
+            this.Controls.Add(this.word4);
+            this.Controls.Add(this.word3);
+            this.Controls.Add(this.word2);
+            this.Controls.Add(this.word1);
+            this.Controls.Add(this.word0);
+            this.Controls.Add(this.slvd);
+            this.Controls.Add(this.slv);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Solver";
+            this.Load += new System.EventHandler(this.Solver_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.corrNum)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -161,6 +270,36 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox entry0;
+        private System.Windows.Forms.TextBox entry1;
+        private System.Windows.Forms.TextBox entry2;
+        private System.Windows.Forms.TextBox entry3;
+        private System.Windows.Forms.TextBox entry4;
+        private System.Windows.Forms.TextBox entry5;
+        private System.Windows.Forms.TextBox entry6;
+        private System.Windows.Forms.TextBox entry7;
+        private System.Windows.Forms.TextBox entry8;
+        private System.Windows.Forms.TextBox entry9;
+        private System.Windows.Forms.TextBox entry10;
+        private System.Windows.Forms.TextBox entry11;
+        private System.Windows.Forms.Button slv;
+        private System.Windows.Forms.Button slvd;
+        private System.Windows.Forms.TextBox word0;
+        private System.Windows.Forms.TextBox word1;
+        private System.Windows.Forms.TextBox word2;
+        private System.Windows.Forms.TextBox word3;
+        private System.Windows.Forms.TextBox word4;
+        private System.Windows.Forms.TextBox word5;
+        private System.Windows.Forms.TextBox word6;
+        private System.Windows.Forms.TextBox word7;
+        private System.Windows.Forms.TextBox word8;
+        private System.Windows.Forms.TextBox word9;
+        private System.Windows.Forms.TextBox word10;
+        private System.Windows.Forms.TextBox word11;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label guessDisplay;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown corrNum;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,28 @@ using System.Windows.Forms;
 
 namespace falloutSolver
 {
-    public partial class Form1 : Form
+    public partial class Solver : Form
     {
-        public Form1()
+        public List<TextBox> boxes;
+        string guess;
+        int tick = 0;
+        Random rand = new Random();
+        public Solver()
         {
             InitializeComponent();
+            boxes = new List<TextBox>();
+            boxes.Add(word0);
+            boxes.Add(word1);
+            boxes.Add(word2);
+            boxes.Add(word3);
+            boxes.Add(word4);
+            boxes.Add(word5);
+            boxes.Add(word6);
+            boxes.Add(word7);
+            boxes.Add(word8);
+            boxes.Add(word9);
+            boxes.Add(word10);
+            boxes.Add(word11);
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -24,14 +42,14 @@ namespace falloutSolver
 
         private void button1_Click(object sender, EventArgs e)
         {
-            String text = textBox1.Text;
-            label1.Text += textBox1.Text + "\n";
+            string text = textBox1.Text.ToUpper();
+            label1.Text += text + "\n";
             textBox1.Clear();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -57,21 +75,106 @@ namespace falloutSolver
 
         private void button4_Click(object sender, EventArgs e)
         {
-            string[] words = label1.Text.Split('\n');
-            Random rand = new Random();
-            int choice = rand.Next(0, words.Length-1);
-            label2.Text = words[choice];
-            label1.Text = "";
-            for (int i = 0; i < words.Length; i++)
-            {
-                if (words[i] != words[choice])
-                {
 
-                }
-            }
         }
 
         private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox9_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void word9_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void word8_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void slv_Click(object sender, EventArgs e)
+        {
+            string[] words = new string[12];
+            for (int i = 0; i < 12; i++)
+            {
+                words[i] = boxes[i].Text.ToUpper();
+            }
+
+            if (tick == 0)
+            {
+                int choice = rand.Next(0, words.Length - 1);
+                guessDisplay.Text = words[choice];
+                boxes[choice].Text = "";
+                guess = words[choice];
+            }
+            else
+            {
+                /*
+             * properties
+             * list of words
+             * number of characters in common with the answer
+             * word that we guess
+             * 
+             * go through each word, and see how many matches per word
+             * list of possible answers
+             * randomly pick an answer
+             * suggest an answer
+             * remove the suggestion and impossible words
+             */
+                List<string> answers = new List<string>();
+                int c = (int)corrNum.Value;
+                //runs through the array of words, find matches by character and determine possible answers
+                for (int i = 0; i < words.Length - 1; i++)
+                {
+                    int matches = 0;
+                    //both are arrays to be used in the for loop for taking guesses in one bank, and words into another
+                    char[] word1 = guess.ToCharArray(); //turns guess into an array of letters
+                    char[] word2 = words[i].ToCharArray(); //turns the current index of words into an array of letters
+                    if (word1.Length == word2.Length)
+                    {
+                        for (int b = 0; b < word1.Length; b++) //looks through each word and picks out characters that match in the array
+                        {
+                            if (word1[b] == word2[b])
+                            {
+                                matches++;
+                            }
+                        }
+                        if (c == matches) //determines possible answers
+                        {
+                            answers.Add(words[i]);
+                        }
+                    }
+                }
+                int choice = rand.Next(0, answers.Count);
+                guessDisplay.Text = answers[choice];
+                guess = answers[choice];
+                answers.RemoveAt(choice);
+            }
+            tick++;
+        }
+
+        private void Solver_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guessDisplay_Click(object sender, EventArgs e)
         {
 
         }
